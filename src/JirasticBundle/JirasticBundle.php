39935be4a8f2c4557f5aa6293a25a34e4d7bdbb0
Jan Friedli <jan.friedli@gmx.ch>
2016-03-01 10:05:02 +0100

    use JirasticBundle

---
+++ src/JirasticBundle/JirasticBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace JirasticBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JirasticBundle extends Bundle
+{
+}
--- src/JirasticBundle/Tests/Controller/DefaultControllerTest.php
+++ src/JirasticBundle/Tests/Controller/DefaultControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AppBundle\Tests\Controller;
+namespace JirasticBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
