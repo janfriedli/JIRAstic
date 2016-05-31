b87379c8b00a3e0e09604e9e1c58fc1e6c8d9bf1
Jan Friedli <jan.friedli@swisscom.com>
2016-05-31 14:26:57 +0200

    use interface for typehinting

--- src/JirasticBundle/EventListener/SecurityLoginListener.php
+++ src/JirasticBundle/EventListener/SecurityLoginListener.php
@@ -6,6 +6,7 @@
 namespace JirasticBundle\EventListener;
 
 use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 use Symfony\Component\HttpKernel\Debug\TraceableEventDispatcher;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
@@ -45,7 +46,7 @@ class SecurityLoginListener
      * @param AuthorizationChecker     $security   Security
      * @param TraceableEventDispatcher $dispatcher Dispatcher
      */
-    public function __construct(Router $router, AuthorizationChecker $security, TraceableEventDispatcher $dispatcher)
+    public function __construct(Router $router, AuthorizationChecker $security, EventDispatcherInterface $dispatcher)
     {
         $this->router = $router;
         $this->security = $security;
