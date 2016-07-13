3cf62264903ac82400549b6113a078ac6d5dc07a
Jan Friedli <jan.friedli@swisscom.com>
2016-07-13 13:21:17 +0200

    fix bug of unique jiraId

--- src/JirasticBundle/Entity/Board.php
+++ src/JirasticBundle/Entity/Board.php
@@ -20,7 +20,6 @@ use Symfony\Component\Validator\Constraints as Assert;
  *
  * @ORM\Table(name="board")
  * @ORM\Entity(repositoryClass="JirasticBundle\Repository\BoardRepository")
- * @UniqueEntity("jiraId")
  */
 class Board
 {
